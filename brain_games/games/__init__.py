30981d8726e0edc456995655e0fd4e6789c32f16
karelinaas <karelinaas95@gmail.com>
2026-04-01 14:19:47 +0300

    Step 9

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -2,6 +2,7 @@ from .calc import calc_game
 from .even import even_game
 from .games import games_game
 from .gcd import gcd_game
+from .prime import prime_game
 from .progression import progression_game
 
 __all__ = [
@@ -9,5 +10,6 @@ __all__ = [
     "even_game",
     "games_game",
     "gcd_game",
+    "prime_game",
     "progression_game",
 ]
---
+++ brain_games/games/prime.py
@@ -0,0 +1,24 @@
+import random
+
+
+def prime_game(game_starts: bool) -> tuple[int, str]:
+    if game_starts:
+        print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    def is_prime(n: int) -> bool:
+        if n < 2:
+            return False
+        if n == 2:
+            return True
+        if n % 2 == 0:
+            return False
+
+        for i in range(3, int(n ** 0.5) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
+    
+    number = random.randint(1, 100)
+    correct_answer = "yes" if is_prime(number) else "no"
+    
+    return number, correct_answer
