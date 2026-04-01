8dfbfda8b133f3d555e1331d223c28c40d7ebf3d
karelinaas <karelinaas95@gmail.com>
2026-04-01 14:42:52 +0300

    Final fixes

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -5,6 +5,7 @@ from prompt import string
 
 
 def welcome_user() -> str:
+    print("Welcome to the Brain Games!")
     name = string("May I have your name? ")
     print(f"Hello, {name}!")
     return name
